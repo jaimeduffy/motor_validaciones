26a568d7f1e91f565c27caddee5e72b442280988
Jaime Duffy <jaime.duffy@nter.es>
2026-01-22 13:54:15 +0100

    Configuración Inicial y archivo properties

--- src/main/scala/org/yiyit/App.scala
+++ src/main/scala/org/yiyit/App.scala
@@ -1,24 +1,48 @@
 package org.yiyit
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import java.util.Properties
 
-/**
- * @author ${user.name}
- */
 object App {
+  def main(args: Array[String]): Unit = {
 
-  def main(args : Array[String]) {
-
-    val spark:SparkSession = SparkSession.builder().master("local[1]")
-      .appName("SparkByExamples.com")
+    // Iniciamos la sesión de Spark
+    val spark = SparkSession.builder()
+      .appName("ValidacionBigData")
+      .master("local[*]")
       .getOrCreate()
-    val rdd:RDD[Int] = spark.sparkContext.parallelize(List(1,2,3,4,5))
-    val rddCollect:Array[Int] = rdd.collect()
-    println("Number of Partitions: "+rdd.getNumPartitions)
-    println("Action: First element: "+rdd.first())
-    println("Action: RDD converted to Array[Int] : ")
-    rddCollect.foreach(println)
-  }
 
+    // Cargamos el archivo de configuración (application.properties)
+    val url = getClass.getResource("/application.properties")
+    if (url == null) {
+      println("ERROR: No se ha encontrado el archivo application.properties en resources")
+      System.exit(1)
+    }
+    val properties = new Properties()
+    properties.load(getClass.getResourceAsStream("/application.properties"))
+
+    // Probamos la conexión leyendo la tabla 'trigger_control'
+    println("--- INICIANDO PRUEBA DE CONEXIÓN A POSTGRESQL ---")
+
+    try {
+      // Leemos la tabla usando los datos del properties
+      val dfTrigger = spark.read.jdbc(
+        properties.getProperty("jdbc.url"),
+        "trigger_control",
+        properties
+      )
+
+      println("¡CONEXIÓN EXITOSA!")
+      println("Mostrando esquema de la tabla trigger_control:")
+      dfTrigger.printSchema()
+
+    } catch {
+      case e: Exception =>
+        println("\nERROR DE CONEXIÓN:")
+        println(e.getMessage)
+        println("Revisa que el Docker esté encendido y el puerto sea correcto.")
+    }
+
+    spark.stop()
+  }
 }
